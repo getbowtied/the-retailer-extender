32db2c7dabd521b648a2dead320013764d0c79e9
Vanesa Reinerth <vanesa@getbowtied.com>
2018-12-10 13:15:33 +0200

    changed blocks icons

--- includes/gbt-blocks/portfolio/block.js
+++ includes/gbt-blocks/portfolio/block.js
@@ -13,13 +13,18 @@
 	const ToggleControl			= wp.components.ToggleControl;
 	const RangeControl			= wp.components.RangeControl;
 	const SelectControl			= wp.components.SelectControl;
+	const SVG 					= wp.components.SVG;
+	const Path 					= wp.components.Path;
 
 	const apiFetch 				= wp.apiFetch;
 
 	/* Register Block */
 	registerBlockType( 'getbowtied/tr-portfolio', {
 		title: i18n.__( 'Portfolio' ),
-		icon: 'format-gallery',
+		icon:
+			el( SVG, { xmlns:'http://www.w3.org/2000/svg', viewBox:'0 0 24 24' },
+				el( Path, { d:'M14 6V4h-4v2h4zM4 8v11h16V8H4zm16-2c1.11 0 2 .89 2 2v11c0 1.11-.89 2-2 2H4c-1.11 0-2-.89-2-2l.01-11c0-1.11.88-2 1.99-2h4V4c0-1.11.89-2 2-2h4c1.11 0 2 .89 2 2v2h4z' } ),
+			),
 		category: 'theretailer',
 		supports: {
 			align: [ 'center', 'wide', 'full' ],
