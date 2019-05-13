285e4b909d9ccbaa17f772660efb2a664d2b18a5
Vanesa Reinerth <vanesa@getbowtied.com>
2019-05-13 18:00:32 +0300

    sliders fix

--- includes/shortcodes/assets/js/wc-products-slider.js
+++ includes/shortcodes/assets/js/wc-products-slider.js
@@ -4,15 +4,22 @@ jQuery(function($) {
 
 	$('.wc-products-slider.swiper-container').each(function() {
 
+		var slides = 4;
+		var medium_slides = 3;
+		if( $(this).parents('.wpb_column').hasClass('vc_span6') || $(this).parents('.wpb_column').hasClass('vc_col-sm-6') ) {
+			slides = 2;
+			medium_slides = 2;
+		}
+
 		var myPostsSwiper = new Swiper($(this), {
-			slidesPerView: 4,
+			slidesPerView: slides,
 			loop: true,
 			breakpoints: {
 				640: {
 			      slidesPerView: 2,
 			    },
 			    959: {
-			      slidesPerView: 3,
+			      slidesPerView: medium_slides,
 			    }
 			},
 			navigation: {
