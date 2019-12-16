0743273744065e005554b82f2549c34c193c0cd0
Vanesa Reinerth <vanesa@getbowtied.com>
2019-12-16 16:21:16 +0200

    v.1.4.4

--- the-retailer-extender.php
+++ the-retailer-extender.php
@@ -4,11 +4,11 @@
  * Plugin Name:       		The Retailer Extender
  * Plugin URI:        		https://theretailer.wp-theme.design/
  * Description:       		Extends the functionality of The Retailer with theme specific features.
- * Version:           		1.4.3
+ * Version:           		1.4.4
  * Author:            		GetBowtied
  * Author URI:        		https://getbowtied.com
  * Requires at least: 		5.0
- * Tested up to: 			5.3
+ * Tested up to: 			5.3.1
  *
  * @package  The Retailer Extender
  * @author   GetBowtied
