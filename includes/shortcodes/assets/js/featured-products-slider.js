4033e949448e1e390293cb52b28a68f817de8b65
Vanesa Reinerth <vanesa@getbowtied.com>
2020-12-15 10:53:42 +0200

    jquery deprecated fixes

--- includes/shortcodes/assets/js/featured-products-slider.js
+++ includes/shortcodes/assets/js/featured-products-slider.js
@@ -1,17 +1,18 @@
 jQuery( function($) {
 
-	$(".featured_products_slider .products_slider_item").mouseenter(function(){
+	$(".featured_products_slider .products_slider_item").on({
+		mouseenter: function(){
+			var that = $(this);
 
-		var that = $(this);
-
-		that.find('.products_slider_infos').stop().fadeTo(100, 0);
-		that.find('.products_slider_images img').stop().fadeTo(100, 0.1, function() {
-			that.find('.products_slider_infos').stop().fadeTo(200, 1);
-		});
-
-	}).mouseleave(function(){
-		$(this).find('.products_slider_images img').stop().fadeTo(100, 1);
-		$(this).find('.products_slider_infos').stop().fadeTo(100, 0);
+			that.find('.products_slider_infos').stop().fadeTo(100, 0);
+			that.find('.products_slider_images img').stop().fadeTo(100, 0.1, function() {
+				that.find('.products_slider_infos').stop().fadeTo(200, 1);
+			});
+		},
+		mouseleave: function(){
+			$(this).find('.products_slider_images img').stop().fadeTo(100, 1);
+			$(this).find('.products_slider_infos').stop().fadeTo(100, 0);
+		}
 	});
 
 	$('.featured_products_slider.swiper-container').each(function() {
